1f4dc695338b4b328e750b593d9a2a16535e6455
sirisha.thumuluri <sirisha.thumuluri@HHCL-CORP-12779.heterohcl.com>
2023-08-05 15:49:51 +0530

    Example One

---
+++ src/com/upendra/Ex.java
@@ -0,0 +1,17 @@
+package com.upendra;
+
+public class Ex {
+	public static void main(String[] args) {
+		System.out.println("example");
+		System.out.println("example");
+		System.out.println("example");
+		System.out.println("example");
+		System.out.println("example");
+		System.out.println("example");
+		System.out.println("example");
+		System.out.println("example");
+		System.out.println("example");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sirisha.thumuluri
+ *
+ */
+module Example {
+}
